6d74f1c34361953aaee6908336764f318a9f918f
Paul Clark <paulclarkaranz@users.noreply.github.com>
2026-02-24 18:31:39 +1300

    refactor(user admin): Tweak tool descriptions.
    My goal here is:
    * reduce the total volume of text - there is some concern (not sure how
      valid it is) that having volume's MCP tool descriptions will take up
      valuable LLM context. When don't want to always use shorthand, this is
      still a human readable text field, but we should aim to be concise
      where possible.
    * don't duplicate information in the type system, arguments and
      responses
    * don't spell out potential errors unless we have to, lets assume the
      LLM can interpret error messages until proven otherwise
    * avoid hardcoding step by step workflows - when we want the LLM to
      combine one tool with another, for example resolving a user provided
      email address to a user ID, it's good to point that out. But hopefully
      we can get away with a single statement and don't have to hold its
      hand.
    
    There was a couple of things I saw that I didn't address. This was just
    a fairly quick pass. Generally I would zero in on wherever we are
    getting into the territory of whole paragraphs. For example
    `add_users_to_instance` still has more text that should be needed IMO.
    
    * get_users_in_instance
      * simplify initial sentence ("user member", "at a time")
      * reduce hint section to bulleted list - reduce context usage. Also add a
        mention of listing the user's roles, very important!
      * add instructions for `count` parameter
      * add return object attributes so the LLM can reason about when it might
        want to call the tool (duplicate the bulleted list?)
    * list_roles_in_instance
      * add "the"
      * simplify args list
        * don't need to tell it how to use a list
        * don't need to tell it what optional/required mean
      * simplify instructions, I don't think we need a step by step instruction
        list of how to combine this tool with others. I think that would defeat
        the flexibility we gain by having the LLM create it's own workflows. So I
        changed this to just say "must match a row in this other tool", I hope it
        can reason that that means it needs to cross check and either get extra
        details or complain about non-existance
      * remove return note about a string error message, it'll see that if it gets
        raised
    * remove_user_from_instance
      * simplify instructions - remove step by step cross reference instructions
      * remove return description of trivial response object - it's self explanatory
    * update_user_role_in_instance
      * remove note stating the obvious about required roles
      * simplify instructions - remove step by step cross reference instructions
      * remove return description of trivial response object - it's self explanatory

--- src/evo_mcp/tools/instance_users_admin_tools.py
+++ src/evo_mcp/tools/instance_users_admin_tools.py
@@ -22,14 +22,20 @@ def register_instance_users_admin_tools(mcp):
     async def get_users_in_instance(
         count: int | None = 10000,
     ) -> list[dict]:
-        """Get all user members in an instance the user is connected to, at a time.
+        """Get all users in the currently selected instance.
        
-        This tool will allow an admin to see who has access to the instance.
-        This tool will also allow admin to see which user does not have access to the instance.
-        Then admin can take action to add or remove users from the instance based on this information.
+        This tool will allow an admin to see:
+            * who has access to the instance,
+            * who does not have access to the instance,
+            * what roles a user has in the instance.
+
+        The admin can take action to add or remove users from the instance based on this information.
+
+        Use the `count` parameter to return only a sample of the users in the
+        instance.
 
         Returns:
-            A list of users in the instance
+            A list of users in the instance, their names, email addresses and roles.
         """
         workspace_client = await get_workspace_client()
 
@@ -77,7 +83,7 @@ def register_instance_users_admin_tools(mcp):
     @mcp.tool()
     async def list_roles_in_instance(
     ) -> list[dict]:
-        """List the roles available in the instance. """
+        """List the roles available in the instance."""
         workspace_client = await get_workspace_client()
 
         instance_roles_response = await workspace_client.list_instance_roles()
@@ -94,31 +100,22 @@ def register_instance_users_admin_tools(mcp):
         Only an instance admin or owner can add users to the instance. If a Forbidden error is returned from add_users_to_instance(), 
         inform the user of the tool that they do not have the required permissions to add users to the instance.
         If a user is already in the instance, an error will be returned - give the error details to the user of the tool
-        and ask user if they wish to update the role of this user. If role update is requested, use `update_user_role_in_instance` tool instead.
+        and ask the user if they wish to update the role of this user. If role update is requested, use `update_user_role_in_instance` tool instead.
         This will help in cases where the user is already in the instance but with a different role, 
         and we want to update the role of the user instead of adding the user again.
         With one request, assign the same role to multiple users by accepting a list of user emails and a list of role IDs.
-        
-        Args:
-            user_emails: List of user email addresses to add. Accept single or multiple emails and make them to a list.
-            Expected to be provided by the user of the tool. Prompt the user to provide email address/addresses if not provided.
 
-                     
-            role_ids: List of role IDs to assign to the users. Must match roles returned by `list_roles_in_instance`. 
-            Following are the instructions regarding role_ids:
-            Ask user of the tool to specify which roles to assign to the user/users. The default role is a read only "Evo user" role.
-            Use `list_roles_in_instance` tool to list available roles in the instance and their corresponding role IDs. 
-            The user can specify one or more role IDs to assign to the user/users.
-            Do not call add_users_to_instance() without providing email and role_ids, as they are required parameters.
-            
-            
+        Args:
+            user_emails: List of user email addresses to add.
+            role_ids: List of role IDs to assign to the users. Must match
+                roles returned by `list_roles_in_instance`. If the user doesn't
+                specify a role, user the "Evo User" role for the selected
+                instance.
+
         Returns:
             A dict with invitations sent and members added.
             Invitations are for external users who would need to accept the invitation to join the instance.
             Members are for users who are already part of the organization.
-            
-            String error message if there was an error adding users.
-            
         """
         workspace_client = await get_workspace_client()
         
@@ -138,24 +135,16 @@ def register_instance_users_admin_tools(mcp):
         user_email: str,
         user_id: UUID 
     ) -> dict|str:
-        """Remove a user from the instance. This will revoke the user's access to the instance.
+        """Remove a user from the selected instance. This will revoke the user's access to the instance.
         Only an instance admin or owner can remove users from the instance. If a Forbidden error is returned from remove_instance_user(), 
         inform the user of the tool that they do not have the required permissions to remove users from the instance. 
-        
         Args:
             user_email: The email address of the user to remove from the instance.
             Do not assume the email address from first name or other information, it should be provided by the user of the tool.
             Prompt the user to provide the email address if not provided. 
 
-            user_id: The user ID of the user to remove from the instance. Following are the instructions to get the user_id:
-            1. Use the tool `get_users_in_instance` to get all users in the instance
-            2. Find the user_id for the given user email. 
-            Pass the user_id to this tool to remove the user from the instance.
-            If the user email does not exist in the instance, return a message saying the user is not in the instance.
-        
-        Returns:
-            A dict with the email of the user removed.
-            
+            user_id: The user ID of the user to remove from the instance. Must
+                match an entry returned from the `get_users_in_instance` tool.
         """
         workspace_client = await get_workspace_client()
 
@@ -173,29 +162,20 @@ def register_instance_users_admin_tools(mcp):
     ) -> dict|str:
         """Update the role of a user in the instance. This will change the user's access level in the instance.
         Only an instance admin or owner can update user roles in the instance. If a Forbidden error is returned from update_instance_user_roles(), 
-        inform the user of the tool that they do not have the required permissions to update user roles in the instance. 
-        If the user does not have the required role, they should not be able to update user roles in the instance.
-
+        inform the user of the tool that they do not have the required permissions to update user roles in the instance.
 
         Args:
-            user_email: The email address of the user to update role for in the instance.
+            user_email: The email address of the user to update the role of.
             Do not assume the email address from first name or other information, it should be provided by the user of the tool.
             Prompt the user to provide the email address if not provided. 
 
-            user_id: The user ID of the user to update role for in the instance. Following are the instructions to get the user_id:
-            1. Use the tool `get_users_in_instance` to get all users in the instance
-            2. Find the user_id for the given user email. 
-            Pass the user_id to this tool to update the user's role in the instance.
-            If the user email does not exist in the instance, return a message saying the user is not in the instance.
+            user_id: The user ID of the user to update the role of. Must match
+                an entry returned by the `get_users_in_instance` tool.
 
-            role_ids: List of role IDs to assign to the user. Following are the instructions regarding role_ids:
-            1. Must match roles returned by `list_roles_in_instance`. 
-            2. Prompt the user to specify which roles to assign. The default role is a read only "Evo user" role.
-            3. Do not call update_instance_user_roles() without providing user_id and role_ids, as they are required parameters.
-
-        Returns:
-            A dict with the email of the user whose role was updated and their new roles.
-            
+            role_ids: List of role IDs to assign to the user. Role IDs must
+                mach an entry returned by the `list_roles_in_instance` tool.
+                The default role is the "Evo user" role for the selected
+                instance.
         """
         workspace_client = await get_workspace_client()
 
